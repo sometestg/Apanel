8e99ea0c48acebf9bb05f114dff2eaff6b5c952c
sometestg <125672753+sometestg@users.noreply.github.com>
2024-05-11 20:28:01 +0700

    Update Protected_26.lua

--- Protected_26.lua
+++ Protected_26.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sometestg/Apanel/main/Protected_33.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/sometestg/Apanel/main/Protected_34.lua"))()
